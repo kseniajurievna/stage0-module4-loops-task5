8652c49c8e5c9b9d01f8c489b3ae25bd2cd9b04b
kseniakaratchenia <62858256+kseniakaratchenia@users.noreply.github.com>
2023-02-22 13:34:53 -0800

    fix4

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-        int interval = 5;
+        int interval = sideLength/2;
         for (int i = 0; i <=sideLength; i++){
             for (int j = 0; j<=sideLength; j++){
                 if(j == interval){
